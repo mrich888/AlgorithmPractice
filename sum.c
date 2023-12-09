84430c9351983a07c54792fcbf95ad62707e9102
mrich888 <850338256@qq.com>
2023-12-09 21:58:56 +0800

    [新增] 新增在数组中找到和为target的两个数的下标

---
+++ sum.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+#define BUFFER_TOTAL 20
+#define RANGE 20
+
+int main()
+{
+    /* 随机数种，保证每次随机出来的数组中的数字不一样 */
+    srand(time(NULL));  
+    /* 定义一个存放随机数的数组 */  
+    int arraryNum[BUFFER_TOTAL];
+    /* 初始化数组 */
+    memset(arraryNum, 0, sizeof(arraryNum));
+    /* 生成随机数组 */
+    for(int idx = 0; idx < BUFFER_TOTAL; idx++)
+    {
+        arraryNum[idx] = rand() % RANGE;
+        //arraryNum[idx] = rand() % TARGET;
+        printf("arrayNum[%d]:%d\n", idx, arraryNum[idx]);
+        
+    }
+    int target;
+    int first_num = 0; //用来存放第一个数
+    int second_num = 0; //用来存放第二个数
+   
+    printf("请输入希望得到的两个数的和：");
+    scanf("%d",&target);
+    int flag = 0;
+    
+    /* 获取第一个数 */
+    for(int idx = 0; idx < BUFFER_TOTAL; idx++)
+    {
+        first_num = arraryNum[idx]; 
+        /* 接着找满足条件的第二个数,并且从第一个数之后开始遍历 */
+        for (int  index = (idx + 1); index < BUFFER_TOTAL; index++)
+        {
+            second_num =  arraryNum[index] ;
+            if(first_num + second_num == target) 
+            {
+                flag = 1;
+                printf("arrayNum[%d]:%d,arrayNum[%d]:%d\n", idx, first_num, index, second_num);
+            }
+        }
+    }
+    if(flag == 0)//用flag记录如果没有符合条件的值则输出以下条件
+    {
+        printf("The array has no matching values.\n");
+    }
+
+    return 0;
+
+}
