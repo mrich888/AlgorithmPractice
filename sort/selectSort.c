0b495087e52c3d5d554809ec856bc97e24c5819d
mrich888 <850338256@qq.com>
2024-01-02 11:21:19 +0800

    [新增] 新增选择排序

--- sort/selectSort.c
+++ sort/selectSort.c
@@ -2,11 +2,43 @@
 
 #define BUFFER_SIZE 7
 
+/* 选择排序：相较于冒泡排序，减少了交换次数 */
+int selectSort(int *array, int length)
+{
+    int ret = 0;
+    int minValue = 0;
+    int minIndex = 0;
+    for (int pos = 0; pos < length; pos++)
+    {
+        int minValue = array[pos];
+        for (int begin = pos + 1; begin < length; begin++)
+        {
+            if (array[begin] > array[begin])
+            {
+                minValue = array[begin];
+                minIndex = begin;
+
+            }
+        }
+        if(array[pos] > minValue)
+        {
+            int temp = array[pos];
+            array[pos] = minValue;
+            array[minIndex] = temp;
+        }
+    }
+
+    return ret;
+}
+
+
 int main()
 {
     int ret = 0;
-    int array[BUFFER_SIZE] ={1, 30, 24, 5, 58, 12, 39};
 
+    int array[BUFFER_SIZE] ={1, 30, 24, 5, 58, 12, 39};
+    int length = sizeof(array) / sizeof(array[0]);
+#if 0
     int min = 0;
     int swappos = 0;
     for(int idx = 0; idx < BUFFER_SIZE; idx++)
@@ -31,8 +63,8 @@ int main()
         }
         printf("array[%d]:%d\n", idx, array[idx]); 
     }    
-
-
+#endif
+    selectSort(array, length);
 
     return 0;
 }
